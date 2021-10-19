7bdbe0699d817bb9b65a651feab7be8abc746042
Pratik Jadhav <pratikjadhav080@gmail.com>
2021-10-20 02:06:34 +0530

    added margin to main div

--- reactmobile/src/App.js
+++ reactmobile/src/App.js
@@ -3,7 +3,7 @@ import {Mobile} from './components/Mobile.js';
 
 function App() {
   return (
-  <div>
+  <div id="maindiv">
     <Mobile list={["Android", "BlackBerry", "Iphone", "Windows Phone"]} name="Mobile Operating System"/>
     <Mobile list={["Samsung", "HTC", "Micromax", "Apple"]} name="Mobile Manufactures"/>
   </div>)
