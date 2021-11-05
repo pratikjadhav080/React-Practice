16e57972c8b24cc39da41f46cbd5a574605f3268
Pratik Jadhav <pratikjadhav080@gmail.com>
2021-11-05 23:59:26 +0530

    made Display restaurant component to accept dynamic list of restaurants

---
+++ restaurantdetails/src/components/DisplayRestaurant.jsx
@@ -0,0 +1,41 @@
+import { Transaction } from "./Transactiontype";
+
+export const DisplayRes = ({ props }) => {
+
+    return (
+    <div className="insidecontainer outsideitem">
+        {props.map((e) => (
+
+            <div className="insideitems container">
+
+                <div className="items">
+                    <img src={e.image}></img>
+                </div>
+
+                <div id="dishdetails" className="items">
+                    <h1 id="title">{e.title}</h1>
+                    <p className="similar">{e.categories.join(", ")}</p>
+                    <p className="similar">Cost ₹{e.cost_for_two} for one</p>
+                    <p className="similarfont">Min ₹{e.cost_for_one} &#8194; <span> &#8226; Up to {e.time} min</span> </p>
+                    <Transaction prop={e.payment_methods} />
+                </div>
+
+                <div id="feedback" className="items">
+                    <h1 id="stars">{e.stars}</h1>
+                    <p className="votes">{e.total_votes} votes</p>
+                    <p className="votes">{e.reviews} reviews</p>
+                </div>
+
+                <div>
+                    <button id="orderbtn" disabled>Order Online</button>
+                </div>
+
+                <div>
+                    <button id="favbtn">Add to favourite</button>
+                </div>
+
+            </div>
+        ))}
+    </div>
+    )
+}
--- restaurantdetails/src/components/Restaurant.jsx
+++ restaurantdetails/src/components/Restaurant.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react"
 import { Decisions } from "./Decisions";
 import '../styles/restaurant.css';
-import { Transaction } from "./Transactiontype";
+
 import { NewRestau } from "./NewRestauModal";
+import { DisplayRes } from "./DisplayRestaurant";
 const data = require('../data/restaurantdata.json');
 
 export const Restaurant = () => {
@@ -14,41 +15,6 @@ export const Restaurant = () => {
         setModal(!modal)
     }
 
-    const dishList = dish.map((e) => {
-        return (
-
-            <div className="insideitems container">
-
-                <div className="items">
-                    <img src={e.image}></img>
-                </div>
-
-                <div id="dishdetails" className="items">
-                    <h1 id="title">{e.title}</h1>
-                    <p className="similar">{e.categories.join(", ")}</p>
-                    <p className="similar">Cost ₹{e.cost_for_two} for one</p>
-                    <p className="similarfont">Min ₹{e.cost_for_one} &#8194; <span> &#8226; Up to {e.time} min</span> </p>
-                    <Transaction prop={e.payment_methods} />
-                </div>
-
-                <div id="feedback" className="items">
-                    <h1 id="stars">{e.stars}</h1>
-                    <p className="votes">{e.total_votes} votes</p>
-                    <p className="votes">{e.reviews} reviews</p>
-                </div>
-
-                <div>
-                    <button id="orderbtn" disabled>Order Online</button>
-                </div>
-
-                <div>
-                    <button id="favbtn">Add to favourite</button>
-                </div>
-
-            </div>
-        )
-    })
-
     return (
         <>
             <div className="outsidecontainer" style={{ filter: modal ? "blur(5px)" : "blur(0px)" }}>
@@ -57,9 +23,8 @@ export const Restaurant = () => {
 
                 <div class="vl outsideitem"></div>
 
-                <div className="insidecontainer outsideitem">
-                    {dishList}
-                </div>
+                <DisplayRes props = {dish}/>
+                
             </div>
 
             {modal ? <NewRestau props={showModal} /> : ""}
