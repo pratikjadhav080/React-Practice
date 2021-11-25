6a553a2490f08c529f628bef4febb7095ae55168
Pratik Jadhav <pratikjadhav080@gmail.com>
2021-11-25 11:12:24 +0530

    counter and todo practice

--- reactredux/src/Store/reducer.js
+++ reactredux/src/Store/reducer.js
@@ -1,29 +1,91 @@
 
-import { ADD_TODO, DEC_COUNT, INC_COUNT } from "./actionTypes";
+import { ADD_TODO_ERROR, ADD_TODO_LOADING, ADD_TODO_SUCCESS, DEC_COUNT, GET_TODO_ERROR, GET_TODO_LOADING, GET_TODO_SUCCESS, INC_COUNT } from "./actionTypes";
 
 
-const initState = { count: 0, todos: [] }
+const initState = {
+    count: 0,
+    todos: {
+        loading: false,
+        data: [],
+        error: false
+    }
+}
 
 export const reducer = (state = initState, { type, payload }) => { //store 1st and action 2nd - order matters
 
     console.log(state)
 
     switch (type) {
+
         case INC_COUNT:
             return {
                 ...state,
                 count: state.count + payload,
             }
+
         case DEC_COUNT:
             return {
                 ...state,
                 count: state.count - payload,
             }
-        case ADD_TODO:
+
+        case ADD_TODO_LOADING:
             return {
                 ...state,
-                todos: [...state.todos, payload],
+                todos: {
+                    ...state.todos,
+                    loading: true,
+                },
             }
+
+        case ADD_TODO_SUCCESS:
+            return {
+                ...state,
+                todos: {
+                    ...state.todos,
+                    loading: false,
+                    data: [],
+                },
+            }
+
+        case ADD_TODO_ERROR:
+            return {
+                ...state,
+                todos: {
+                    ...state.todos,
+                    loading: false,
+                    error: true,
+                },
+            }
+
+        case GET_TODO_LOADING:
+            return {
+                ...state,
+                todos: {
+                    ...state.todos,
+                    loading: true,
+                },
+            }
+
+        case GET_TODO_SUCCESS:
+            return {
+                ...state,
+                todos: {
+                    loading: false,
+                    data: payload,
+                },
+            }
+
+        case GET_TODO_ERROR:
+            return {
+                ...state,
+                todos: {
+                    ...state.todos,
+                    loading: false,
+                    error: true,
+                },
+            }
+
         default:
             return state;
     }
--- reactredux/src/components/Todo.jsx
+++ reactredux/src/components/Todo.jsx
@@ -1,36 +1,68 @@
 import { Actions } from '../Store/actions';
 import { useDispatch, useSelector } from 'react-redux';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid'
-import { ADD_TODO } from '../Store/actionTypes';
+import { ADD_TODO_ERROR, ADD_TODO_LOADING, ADD_TODO_SUCCESS, GET_TODO_ERROR, GET_TODO_LOADING, GET_TODO_SUCCESS } from '../Store/actionTypes';
+import axios from "axios"
 
 export const Todo = () => {
 
-    const [todo,setTodo] = useState("");
+    const [todo, setTodo] = useState("");
     const dispatch = useDispatch();
-    const todos = useSelector(store => store.todos);
+    const { loading, data, error } = useSelector(store => store.todos);
 
-    const addTolist = () =>{
 
-        const data = {
-            id:nanoid(4), 
-            title : todo,
-            status:false
+    useEffect(() => {
+        getData()
+    },[])
+
+    const getData = async ()=>{
+        dispatch(Actions(GET_TODO_LOADING, ""))
+
+        try{
+            const res = await axios.get("http://localhost:3004/todos")
+            dispatch(Actions(GET_TODO_SUCCESS, res.data)) 
+        }catch(err){
+            dispatch(Actions(GET_TODO_ERROR, err)) 
         }
-
-        dispatch(Actions(ADD_TODO, data))
     }
 
-    return <div>
-        <input type="text" value={todo} onChange={(e)=>setTodo(e.target.value)}/>
+    const addTolist = async () => {
 
-        <button onClick={addTolist}>Add TODO</button>
+        dispatch(Actions(ADD_TODO_LOADING, ""))
 
-        {todos.map((e) => {
-           return <div key={e.id}>
-               <h1>{e.title}</h1>
-           </div>
-        })}
+        const payload = {
+            id: nanoid(4),
+            title: todo,
+            status: false
+        }
 
-    </div>
+        try {
+            const res = await axios.post("http://localhost:3004/todos", payload)
+            dispatch(Actions(ADD_TODO_SUCCESS, res.data))
+            getData();
+        } catch (err) {
+            dispatch(Actions(ADD_TODO_ERROR, err))
+        }
+
+    }
+
+    return loading ?
+
+        (<h1>Loading ...</h1>) : error ? (<h1>error</h1>) : (<div>
+
+            <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
+
+            <button onClick={addTolist}>Add TODO</button>
+
+            {data.map((e) => {
+                return <div key={e.id}>
+                    <h1>{e.title}</h1>
+                </div>
+            })}
+
+        </div>
+        )
 }
+
+//error ? <h1>{error}</h1> : 
