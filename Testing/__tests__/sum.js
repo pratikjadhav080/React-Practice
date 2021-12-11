8f86b9fbe742f419ab896e8ab9b9ab73a09b4e33
Pratik Jadhav <pratikjadhav080@gmail.com>
2021-12-11 14:15:20 +0530

    tests added for substraction

--- Testing/__tests__/sum.js
+++ Testing/__tests__/sum.js
@@ -5,6 +5,12 @@ const Add = require("../index")
 test("addition of two positive numbers",function(){
     expect(Add(1,2)).toBe(3)
     expect(Add(2,2)).toBe(4)
-    expect(Add(2,6)).toBe(8)
-    expect(Add(2,1)).toBe(3)
+})
+
+test("addition of two negataive numbers",function(){
+    expect(Add(-1,-2)).toBe(-3)
+})
+
+test("addition of one postive one negataive number",function(){
+    expect(Add(-1,2)).toBe(1)
 })
